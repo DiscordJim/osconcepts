3e13d9aec2f65caf55eb50333046566cfc823616
Homer Riva-Cambrin <neithercouldterry@gmail.com>
2024-12-16 19:16:13 -0700

    added logical translation

--- src/memory/paging/local.rs
+++ src/memory/paging/local.rs
@@ -5,16 +5,31 @@ use super::Page;
 
 /// The logical address has the first 6 bits set
 /// to the page number and then the last 10 bits
+/// 
+/// This is designed for little endian.
 pub struct LogicalAddress(usize);
 
 impl LogicalAddress {
-    pub fn create(page: *const Page) {
+    pub fn create(page: *const Page) -> (u16, Self) {
         // Generate a local header address.
         let local_root: u16 = random();
 
-        // Extract the first 16-bits of the address.
-        let real_root: u16 = (((page as usize) & !(!0 >> 16)) >> 48) as u16;
+        let page_number = page as usize;
 
+        // Extract the real root.
+        let clear_mask = (!0 as usize) << 16;
+        let real_root: u16 = (page_number & !clear_mask) as u16;
+
+        // Create the modified address
+        let logical = (page_number & clear_mask) | (local_root as usize);
+
+
+        (real_root, Self(logical))
+    }
+    pub fn translate(&self, real_root: u16) -> *const Page {
+        // Recreate the actual pointer address.
+        let real = (self.0 & ((!0 as usize) << 16)) | (real_root as usize);
+        real as *const Page
     }
 }
 
@@ -32,36 +47,54 @@ mod tests {
         let mut allocator = PageAllocator::new(1);
         let page_ptr = allocator.acquire();
 
-        let logical = LogicalAddress::create(page_ptr);
-        let page_ptr_us = page_ptr as usize;
+        let (root, log) = LogicalAddress::create(page_ptr);
+        
+        assert_eq!(log.translate(root) as usize, page_ptr as usize);
+        // let page_ptr_us = page_ptr as usize;
 
         
-        println!("Ptr:  {:#066b}", page_ptr_us);
+        // println!("Ptr:  {:#066b}", page_ptr_us);
+        // // let mask = ;
+        // let real = (log.0 & ((!0 as usize) << 16)) | (root as usize);
+        // assert_eq!(page_ptr_us, real);
 
 
-        let faux: u16 = random();
-        println!("Locl: {:#018b}", faux);
 
+
+        // let faux: u16 = random();
+        // println!("Locl: {:#018b}", faux);
+
+        // // Extract the real root.
+        // let clear_mask = (!0 as usize) << 16;
+        // let real: u16 = ((page_ptr_us & !(clear_mask))) as u16;
+
+        // let log_ptr = (page_ptr_us & clear_mask) | (faux as usize);
+      
+        // println!("Real: {:#018b}", real);
+        // println!("Lgcl: {:#066b}", log_ptr);
+
+        // let back = (log_ptr & clear_mask) | (real as usize);
+        // assert_eq!(page_ptr_us, back);
      
 
 
-        // This will extract the real first 16-bits of the address.
-        let real: usize = page_ptr_us & !(!0 >> 16);
-        let real_root: u16 = (real >> 48) as u16;
+        // // This will extract the real first 16-bits of the address.
+        // let real: usize = page_ptr_us & !(!0 >> 16);
+        // let real_root: u16 = (real >> 48) as u16;
 
 
-        // Calculate the local address
-        let local: usize = (faux as usize) << 48;
-        let logical: usize = local | (page_ptr_us & (!0 >> 16));
+        // // Calculate the local address
+        // let local: usize = (faux as usize) << 48;
+        // let logical: usize = local | (page_ptr_us & (!0 >> 16));
         
 
 
-        println!("Real: {:#066b}", real);
-        println!("Faux: {:#066b}", local);
-        println!("Lgcl: {:#066b}", logical);
+        // println!("Real: {:#066b}", real);
+        // println!("Faux: {:#066b}", local);
+        // println!("Lgcl: {:#066b}", logical);
 
         
-        println!("Reas: {:#018b}", real_root);
+        // println!("Reas: {:#018b}", real_root);
 
 
         
